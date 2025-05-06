5a7026c96a4bf9bab4d6c2cc389d7d8de2e6ce05
Xavi <xavier.caricchio@gmail.com>
2025-05-06 14:47:40 +0200

    queue prefetcher

--- prefetcher/mypref/mypref.cc
+++ prefetcher/mypref/mypref.cc
@@ -1,13 +1,165 @@
 #include "mypref.h"
 
+#include "cache.h"
+
+// 0:1 1:0 2:-1
+// 3:-1 0:2
+// 190:1
+
+// ip st 0x1000 10 3
+// 1000
+// st 0x2000+4i 11 3
+// 1000
+// ip st 0x1020 10 3
+// 2
+// st 0x2020+4i 3
+// 2
+// ip st 0x1040 10
+
 uint32_t mypref::prefetcher_cache_operate(champsim::address addr, champsim::address ip, uint8_t cache_hit, bool useful_prefetch, access_type type,
                                       uint32_t metadata_in)
 {
-  // assert(addr == ip); // Invariant for instruction prefetchers
+  champsim::block_number cl_addr{addr};
+  champsim::block_number::difference_type stride = 0;
+  uint8_t degree = 1;
+
+  auto foundR1 = R1buffer.find((random_entry){.addr=addr});
+  auto foundR2 = R2buffer.find((random_entry){.addr=addr});
+  auto foundR3 = R3buffer.find((random_entry){.addr=addr});
+  auto foundO  = Otable.check_hit((random_entry){.addr=addr});
+
+  // check if addr has correlated block on object table
+  if (foundO.has_value()) {
+    object_lookahead = {addr, foundO->stride};
+  }
+
+  int strideR1 = -1;
+  int strideR2 = -2;
+  int strideR3 = -3;
+  if (foundR1.has_value()) strideR1 = foundR1->stride;
+  if (foundR2.has_value()) strideR2 = foundR2->stride;
+  if (foundR3.has_value()) strideR3 = foundR3->stride;
+
+  int strideO = 0;
+  if(strideR1 == strideR2) strideO = strideR1;
+  if(strideR1 == strideR3) strideO = strideR1;
+  if(strideR2 == strideR3) strideO = strideR2;
+
+  if (strideO != 0) {
+    Otable.fill(
+      (random_entry){.addr=addr, .stride=strideO}
+    );
+  }
+
+  if (R3_entry.stride != 0)
+    R3buffer.insert(R3_entry);
+
+  if (R2_entry.stride != 0) {
+    if (R2buffer.full())
+      R3_entry = R2buffer.last();
+    R2buffer.insert(R2_entry);
+  }
+
+  if (R1_entry.addr != addr) {
+    if (R1buffer.full())
+      R2_entry = R1buffer.last();
+    R1buffer.insert((random_entry){.addr=addr, .stride=champsim::offset(addr, R1_entry.addr)});
+  }
+
+  R1_entry.addr = addr;
+
+  auto found = table.check_hit((tracker_entry){.addr=ip}); 
+
+  // if we found a matching entry
+  if (found.has_value()) {
+    if (found->type==tracker_entry::entry_type::SEQUENTIAL) {
+
+      stride = champsim::offset(found->sequential.last_cl_addr, cl_addr);
+      degree = found->sequential.degree;
+      
+      if (active_lookahead.has_value()) {
+        auto [pf_ip, pf_address, pf_stride, pf_degree] = active_lookahead.value();
+        if (pf_ip == ip) {
+          if (pf_address == addr && pf_degree == 0) {
+            degree +=1;
+            active_lookahead = {pf_ip, pf_address, pf_stride, 1};
+          }
+        } else {
+          if (pf_degree != 0) {
+            // update last entry of prefetch
+            auto last_prefetch = table.check_hit(
+              (tracker_entry){
+                .type=tracker_entry::entry_type::SEQUENTIAL,
+                .addr=pf_ip, 
+                //.sequential={cl_addr, stride, degree}
+              }
+            );
+            table.fill(
+              (tracker_entry){
+                .type=tracker_entry::entry_type::SEQUENTIAL,
+                .addr=last_prefetch->addr, 
+              .sequential={last_prefetch->sequential.last_cl_addr, last_prefetch->sequential.last_stride, last_prefetch->sequential.degree-1}
+            }
+            );
+          }
+        }
+      } 
+        // Initialize prefetch state unless we somehow saw the same address twice in
+        // a row or if this is the first time we've seen this stride
+        else if (stride != 0 && stride == found->sequential.last_stride)
+        active_lookahead = {ip, champsim::address{cl_addr}, stride, degree}; // PREFETCH_DEGREE};
+    } else {
+
+    }
+  }
+
+  // update tracking set
+  table.fill(
+    (tracker_entry){
+      .type=tracker_entry::entry_type::SEQUENTIAL,
+      .addr=ip, 
+      .sequential={cl_addr, stride, degree}
+    }
+  );
+
   return metadata_in;
+}
+
+void mypref::prefetcher_cycle_operate()
+{
+  // If a lookahead is active
+  if (active_lookahead.has_value()) {
+    auto [ip, old_pf_address, stride, degree] = active_lookahead.value();
+    // assert(degree > 0);
+    
+    champsim::address pf_address{champsim::block_number{old_pf_address} + stride};
+
+    // If the next step would exceed the degree or run off the page, stop
+    if (intern_->virtual_prefetch || champsim::page_number{pf_address} == champsim::page_number{old_pf_address}) {
+      // check the MSHR occupancy to decide if we're going to prefetch to this level or not
+      const bool mshr_under_light_load = intern_->get_mshr_occupancy_ratio() < 0.5;
+      const bool success = prefetch_line(pf_address, mshr_under_light_load, 0);
+      if (success)
+        active_lookahead = {ip, pf_address, stride, degree - 1};
+      // If we fail, try again next cycle
+
+      // if (active_lookahead->degree == 0) {
+      //   active_lookahead.reset();
+      // }
+    } else {
+      active_lookahead.reset();
+    }
+  }
 }
 
 uint32_t mypref::prefetcher_cache_fill(champsim::address addr, long set, long way, uint8_t prefetch, champsim::address evicted_addr, uint32_t metadata_in)
 {
+  // leer table
+  // 0: addr1 <-- 1, 
+  // 1: addr2 <-- 2, 4 
+  // 2: addr3 <-- 3, 0
+  // addr1 <-- addr2 M[addr1] <- addr2
+  // addr2 <-- 
+  
   return metadata_in;
 }
--- prefetcher/mypref/mypref.h
+++ prefetcher/mypref/mypref.h
@@ -1,13 +1,213 @@
+/*
+Axiom
+- A cache line may be related with a set of cache lines.
+
+Consequences
+- A cache line may be related with sequencials cache lines.
+- A cache line may not be related with any cache line.
+
+Idea:
+
+Strong table
+  - Unified table that shares space between sequential and random cache blocks
+  - Represents the tendency of a program
+  - LRU
+Weak sequential table
+  
+Weak random table
+
+*/
+
+
 #ifndef PREFETCHER_MYPREF_H
 #define PREFETCHER_MYPREF_H
 
 #include <cstdint>
+#include <optional>
+#include <bitset>
 
+#include "address.h"
 #include "champsim.h"
 #include "modules.h"
+#include "msl/lru_table.h"
+/* MAIN IDEAS    */
+// Es importante no saturar el bus??
+// Prefech depending on jump in loops         // 16 kb
+// ip -> addr
+//
+
+// Table address to address LRU   // 32 kb
+// 
+//
+//
+
+// Prefetch degree variable ( hit rate )
+
+// 1 hit -> 2,3,4
+//                | 2,3,4
+// Next line if no override data
+
+// miss -> addr_miss, last_addr_miss -> (last_addr_m, addr_m), last_addr_miss := addr_m
+//
+//
+/*
+  Objects:
+    IPtable:    stores ip and strides info for the sequence
+    Rtable[N]:  stores pair of address (addr1, b(addr2))
+    Otable:     store correlated pair of address
+
+    empty state:
+      I(ip, b(addr), 0, 3)
+      R(addr, _)
+    
+    next:
+      R(addr, b(addr2), degree) if (p(addr) == p(addr2))
+      R(addr2, _)
+
+      addr
+      R(addr, b(addr2), degree) if (p(addr) == p(addr2))
+
+
+    Rtable      Rtable      Rtable   | Otable
+    (x,y)       (x,y)                | (x,y)
+
+  tag: ip & addr
+
+    LRU is ip?
+      then:      LRU ip table  -> ip, b(addr), found.b - b(addr), (b(addr) - fond.b  == found.stride ? found.degree+1 : found.degree)
+      otherwise: 
+        LRU ip table  -> ip, b(addr), 0, 1
+        if lookahead.degree > 0 then LRU 
+    LRU recent addr -> addr & b(addr)
+
+  tag: ip & addr
+
+
+    LRU is addr?
+
+
+
+*/
+// M[prev_miss] <- addr
+// prev_miss <- addr
+
+template <typename T, unsigned Sz>
+class queue {
+
+  T _data[Sz];
+  int num;
+  public:
+  queue();
+
+  void clear() { num=0; }
+  bool empty() { return num == 0;}
+  bool full() { return num == Sz;}
+
+  T& last() { _data[num-1]; }
+
+  std::optional<T> find(T& elem) {
+    for(int i=0; i<num; ++i) {
+      if (_data[i].tag() == elem.tag()) {
+        return std::optional(_data[i]);
+      }
+    }
+    return std::optional<T>();
+  }  
+
+  void insert(T& elem) {
+    for(int i=0; i < num-1; ++i) {
+      _data[i+1] = _data[i];
+    }
+    _data[0] = elem;
+    if (num != Sz) ++num;
+  }
+};
 
 class mypref : public champsim::modules::prefetcher
 {
+
+  struct random_entry {
+
+    champsim::address addr{};
+    champsim::block_number::difference_type stride{};
+
+    auto index() const
+    {
+      using namespace champsim::data::data_literals;
+
+      return addr.slice_upper<2_b>();
+    }
+    auto tag() const
+    {
+      using namespace champsim::data::data_literals;
+      
+      return addr.slice_upper<2_b>();
+    }
+  };
+
+  struct tracker_entry {
+
+    enum entry_type {
+      SEQUENTIAL, RANDOM
+    };
+    
+    struct sequential_entry {
+      champsim::block_number last_cl_addr{};                 // the last address accessed by this IP
+      champsim::block_number::difference_type last_stride{}; // the stride between the last two addresses accessed by this IP
+      uint8_t degree;
+    };
+
+    struct random_entry {
+      champsim::block_number::difference_type stride{};
+      uint8_t trust;   // trust on the decisition
+    };
+    
+    // tag
+    champsim::address addr{};                              // the IP we're tracking
+    // data
+    entry_type type;                                      
+    union {
+      sequential_entry sequential;
+      random_entry random;
+    };
+
+
+    auto index() const
+    {
+      using namespace champsim::data::data_literals;
+
+      return addr.slice_upper<2_b>();
+    }
+    auto tag() const
+    {
+      using namespace champsim::data::data_literals;
+      
+      return addr.slice_upper<2_b>();
+    }
+  };
+
+  struct lookahead_entry {
+    champsim::address ip{};
+    champsim::address address{};
+    champsim::address::difference_type stride{};
+    uint8_t degree = 0; // degree remaining
+  };
+
+  constexpr static std::size_t TRACKER_SETS = 256;
+  constexpr static std::size_t TRACKER_WAYS = 4;
+  constexpr static std::size_t RTABLE_NUM =  4;
+
+  std::optional<lookahead_entry> active_lookahead;
+  std::optional<random_entry> object_lookahead;
+
+  champsim::msl::lru_table<tracker_entry> table{TRACKER_SETS, TRACKER_WAYS};
+
+  champsim::msl::lru_table<random_entry> Otable{TRACKER_SETS, TRACKER_WAYS};
+  queue<random_entry, TRACKER_SETS> R1buffer;
+  queue<random_entry, TRACKER_SETS> R2buffer;
+  queue<random_entry, TRACKER_SETS> R3buffer;
+
+  random_entry R1_entry, R2_entry, R3_entry;
 public:
   using prefetcher::prefetcher;
 
@@ -16,7 +216,7 @@ public:
   uint32_t prefetcher_cache_operate(champsim::address addr, champsim::address ip, uint8_t cache_hit, bool useful_prefetch, access_type type,
                                     uint32_t metadata_in);
   uint32_t prefetcher_cache_fill(champsim::address addr, long set, long way, uint8_t prefetch, champsim::address evicted_addr, uint32_t metadata_in);
-  // void prefetcher_cycle_operate() {}
+  void prefetcher_cycle_operate();
   // void prefetcher_final_stats() {}
 };
 
